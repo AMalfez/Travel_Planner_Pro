0aff39b7c5183ec28e2e1054c98b5be1af6092c8
AMalfez <95576731+AMalfez@users.noreply.github.com>
2023-07-30 13:54:16 +0000

    Real place name

--- travel-app/src/Card.js
+++ travel-app/src/Card.js
@@ -10,7 +10,7 @@ export default function Card({data}) {
                  <img src={data.result_object.photo.images.small.url} alt='an imge' />
                 </div>
                 <div className='card_details'>
-                    <p style={{fontSize:'25px', fontWeight:'600'}}>{data.result_object.location_string} <span className='edit-icon'>Edit icon</span></p>
+                    <p style={{fontSize:'25px', fontWeight:'600'}}>{data.result_object.name} <span className='edit-icon'>Edit icon</span></p>
                     <p style={{ color:'lightgrey'}}>{data.result_object.category.name}</p>
                     <p><span><Star rating={data.result_object.rating}/></span></p>
                     <p>details</p>
