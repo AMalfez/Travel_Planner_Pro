f424423251d9b3a65ba3eb091794a19b5a556c9b
AMalfez <95576731+AMalfez@users.noreply.github.com>
2023-07-30 09:53:15 +0000

    api exhausted

--- travel-app/src/Testing.js
+++ travel-app/src/Testing.js
@@ -1,25 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
-const URL = 'https://ai-trip-planner.p.rapidapi.com/';
 
-const options = {
+
+function Testing() {
+  const [location, setLocation] = useState('London,UK');
+  const [days, setDays] = useState('3');
+  const URL = 'https://ai-trip-planner.p.rapidapi.com/';
+  const options = {
     params: {
-      days: '3',
-      destination: 'London,UK'
+      days: days,
+      destination: location
     },
     headers: {
       'X-RapidAPI-Key': '4ca9bb3b84mshac2b5b891850fa4p1a5428jsn03c84aa2cd6c',
       'X-RapidAPI-Host': 'ai-trip-planner.p.rapidapi.com'
     }
   };
-function Testing() {
+
     const getTesting=async()=>{
         const result = await axios.get(URL, options); 
         console.log(result.data);
     }
+
+    const handleLocation = (e)=>{
+      setLocation(`${e.target.value}`);
+    }
+    const handleDays =(e)=>{
+      setDays(`${e.target.value}`)
+    }
+    const handleSubmit=async(e)=>{
+      e.preventDefault();
+      const result = await axios.get(URL, options); 
+      console.log(result.data);
+    }
   return (
     <div>
-        <button onClick={getTesting}>Click</button>
+        {/* <button onClick={getTesting}>Click</button> */}
+        <form onSubmit={handleSubmit}>
+          <input type='text' onChange={handleLocation} value={location} name='location' placeholder='Destination' />
+          <input type='text' onChange={handleDays} value={days} name='Days' placeholder='Days' />
+          <input type='submit' value='Submit Me' />
+        </form>
     </div>
   )
 }
